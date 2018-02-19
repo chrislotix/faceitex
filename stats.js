471f16fa9156961bb643ce8f9c4b26619fcf58ea
chrislotix <chrislotix@gmail.com>
2018-02-19 19:12:54 +0100

    Add comment

--- stats.js
+++ stats.js
@@ -18,21 +18,28 @@ var kills   = 0;
 var assists = 0;
 var deaths  = 0;
 
-
+//
+// Extracts text from all the spans on profile page
+//
 function extractTextFromSpan() {
   for(var item of statsSpan) {
     extractedText.push(item.textContent.replace(/\s/g, ''));
   }
 }
 
-
+//
+// Fills the stats with the KAD stats in array format [K,A,D,KD]
+//
 function getStatsFromExtractedText() {
   for(var element of extractedText) {
     statsArray.push(element.match(/[\d]*[\.]{0,1}[\d]+/g));
   }
 }
 
-
+//
+// Assigns values to the variables from statsArray
+// TODO: Replace the vars with player object
+//
 function getAllStats() {
   for(var i = 0; i < statsArray.length; i++) {
     kills   += parseInt(statsArray[i][0]);
@@ -41,13 +48,22 @@ function getAllStats() {
   }
 }
 
+//
+// Helper function to get the kill:death ratio.
+// It will break when player objects are introduced
+// and kill deaths variables are removed / replaced.
+//
 function getKillDeathRatio() {
   return (kills/deaths).toFixed(2);
 }
 
-extractTextFromSpan();
-getStatsFromExtractedText();
-getAllStats();
+function init() {
+  extractTextFromSpan();
+  getStatsFromExtractedText();
+  getAllStats();
+}
+
+init();
 
 var message = "K: " + kills + " " +
               "A: " + assists + " " +
